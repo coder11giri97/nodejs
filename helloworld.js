edef81141f3766262532e18e8355bbf2b73d01c0
coder11giri97 <girishnakate19@gmail.com>
2022-10-25 15:33:39 +0530

    added server

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,25 @@
 console.log("hello world")
+
+// const getProduct = (a, b) => a*b;
+
+// console.log(getProduct(3,9))
+
+
+
+// const student = {
+
+//     name: "giri",
+
+//     age: 24,
+
+//     getInfo() {
+
+//         console.log(`My name is ${this.name} and i am ${this.age} years old`)
+
+//     }
+
+// }
+
+
+
+// student.getInfo();
