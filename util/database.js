d41493eb841edc3c3e857c2254337f1812ba274c
coder11giri97 <girishnakate19@gmail.com>
2022-10-30 15:25:32 +0530

    data retrieval

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Zeus@007",
+    password: "apple@17",
     database: "node_intro"
 })
 
